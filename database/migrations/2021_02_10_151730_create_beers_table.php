ab5173aace07cbbeed3303f3df29dc9a41727cac
Leonardo <leonardo.ranaldo.b@gmail.com>
2021-02-10 16:44:09 +0100

    cerazione model e database

---
+++ database/migrations/2021_02_10_151730_create_beers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 30);
+            $table->string('tipo', 30);
+            $table->string('paese', 20);
+            $table->string('regione', 20);
+            $table->float('gradazione_alcolica', 4, 1);
+            $table->longText('descrizione');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
